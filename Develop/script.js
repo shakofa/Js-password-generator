2fc4c40664717fa35abee511d8ac0a7ef2b54607
Anahita <shakofa.mahmody@gmail.com>
2020-12-21 00:20:59 -0800

    Add a function and prompt and confirm variablse to js file.

--- Develop/script.js
+++ Develop/script.js
@@ -89,7 +89,69 @@ var confirmLowerCase;
 var confirmSpecialChar;
 
 
+//Start Function to get user password options
+function passwordOptions(){
+  //ask for user input
+  var length = parseInt(prompt('How many Characters would you like your password?'));
 
+
+  if(!length){
+    alert('This needs a value.');
+    return;
+  } 
+
+
+  //Check if the password lenght is a number.
+  if (isNaN(lenght) === true) {
+    alert('password lenght must be provided as a number');
+    return;
+  }
+  
+ if( length < 8 || length > 128){
+    //Start user input prompts
+    length = parseInt(prompt('You must choose between 8 and 128'));
+  }
+
+
+  //User input variables that store boolean for inclusion of numeric and special Charecters and uppercase and lowercase letters
+  var confirmSpecialChar = confirm('Click OK to add a special character.');
+  var confirmUpperCase = confirm('Click OK to add an uppercase character.');
+  var confirmLowerCase = confirm('Click OK to add a lowercase character.');
+  var confirmNumericChar = confirm('Click OK to add a numeric character.');
+
+
+  if(
+    confirmSpecialChar === false &&
+    confirmUpperCase === false &&
+    confirmLowerCase === false &&
+    confirmNumericChar === false
+  ){
+    alert('You must select at last one Character type.');
+    return;
+  }
+
+
+// Object to store user input
+var userOption = {
+  lenght: length,
+  confirmSpecialChar: confirmSpecialChar,
+  confirmUpperCase: confirmUpperCase,
+  confirmLowerCase: confirmLowerCase, 
+  confirmNumericChar: confirmNumericChar,
+};
+    
+  return userOption;
+}
+
+  
+
+// Function for getting a random element from an array
+function getRandom(arr) {
+  var randIndex = Math.floor(Math.random() * arr.length);
+  var randElement = arr[randIndex];
+
+  return randElement;
+}
 
 
 
